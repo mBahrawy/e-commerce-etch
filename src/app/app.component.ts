eacfaf2242bdbf9a23dd9cc249abf54cce3f9eff
Mohamed Bahrawy <bahrawy.dev@gmail.com>
2026-02-20 21:05:57 +0200

    bug fixing and refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LanguageService } from '../core/services/language.service';
+import { LanguageService } from './core/services/language.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: '<router-outlet />',
 })
 export class AppComponent {
   private readonly languageService = inject(LanguageService);
